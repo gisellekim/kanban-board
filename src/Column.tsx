cba5a346ea03257fddf770ac96e072a35d137db5
giselle <giselle.kim@eyos.one>
2023-12-08 00:04:14 +0000

    feat: build a draggable column layout using react-dnd and throttle-debounce-ts

--- src/Column.tsx
+++ src/Column.tsx
@@ -2,7 +2,11 @@ import { ColumnContainer, ColumnTitle } from "./styles"
 import { Card } from "./Card"
 import { AddNewItem } from "./AddNewItem"
 import { useAppState } from "./state/AppStateContext"
-import { addTask } from "./state/actions"
+import { addTask, moveList } from "./state/actions"
+import { useRef } from "react"
+import { useItemDrag } from "./utils/useItemDrag"
+import { throttle } from "throttle-debounce-ts"
+import { useDrop } from "react-dnd"
 
 type ColumnProps = {
   id: string
@@ -10,11 +14,28 @@ type ColumnProps = {
 }
 
 export const Column = ({ id, text }: ColumnProps) => {
-  const { getTasksByListId, dispatch } = useAppState()
-
+  const { draggedItem, getTasksByListId, dispatch } = useAppState()
   const tasks = getTasksByListId(id)
+  const ref = useRef<HTMLDivElement>(null)
+  const { drag } = useItemDrag({ type: "COLUMN", id, text })
+  const [, drop] = useDrop({
+    accept: "COLUMN",
+    hover: throttle(200, () => {
+      if (!draggedItem) {
+        return
+      }
+      if (draggedItem.type === "COLUMN") {
+        if (draggedItem.id === id) {
+          return
+        }
+        dispatch(moveList(draggedItem.id, id))
+      }
+    }),
+  })
+  drag(drop(ref))
+
   return (
-    <ColumnContainer>
+    <ColumnContainer ref={ref}>
       <ColumnTitle>{text}</ColumnTitle>
       {tasks.map((task) => (
         <Card key={task.id} id={task.id} text={task.text} />
